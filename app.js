74ea8bd8d9b2710ac3869f229fbdbe1bd3d964c9
lizziekarr <lizziemoves@gmail.com>
2017-07-25 21:31:28 -0500

    add LikeController and background image

--- app.js
+++ app.js
@@ -7,11 +7,31 @@ app.directive('newspaperHeading', Heading)
    .directive('businessArticle', Business)
    .directive('foodArticle', Food);
 
-   function Heading(){
-     return {
-       template: '<h1>The Monday Times</h1>'
-     }
+app.controller('LikeController', LikeController);
+
+LikeController.$inject = ['$scope'];
+function LikeController($scope){
+  $scope.foodLikes = 0;
+  $scope.businessLikes = 0;
+  $scope.fashionLikes = 0;
+  $scope.addLike = function(type){
+    if (type === 'food'){$scope.foodLikes++;}
+    else if (type === 'business'){$scope.businessLikes++;}
+    else if (type === 'fashion'){$scope.fashionLikes++;}
+  };
+}
+
+/*LikeController.$inject = ['$scope'];
+function LikeController($scope){
+
+
+}*/
+
+ function Heading(){
+   return {
+     template: '<h1>The Monday Times</h1>'
    }
+ }
 
 function Fashion(){
   return {
